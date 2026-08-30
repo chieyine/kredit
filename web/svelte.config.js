154a64ce296bcb0453ffcb7d7728e33e792811f3
chieyine <chieyinenelson@gmail>
2026-08-30 22:20:08 +0100

    build: configure adapter-auto for Vercel deployment

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ precompress: true }),
+		adapter: adapter(),
 		alias: {
 			$features: 'src/lib/features',
 			$components: 'src/lib/components'
